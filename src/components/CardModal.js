ec809a0e8655afae235ccb058079a792b3166962
Masoud <mas.sam1368@gmail.com>
2022-09-12 16:46:22 +0430

    finish styling

--- src/components/CardModal.js
+++ src/components/CardModal.js
@@ -1,20 +1,49 @@
+import styles from './CardModal.module.css'
+
 const CardModal = () => {
   return (
-    <div style={{ 'display': 'none' }}>
+    <div className={styles.cardModal} style={{ 'display': 'block' }}>
 
-      <div></div>
+      <div className={styles.transparent}>sdf</div>
 
-      <div>
-        <div>
-          <h3>Sushi</h3>
-          <span>$22.99</span> <span>3</span>
+      <div className={styles.CardModalInner}>
+        <ul>
+          <li>
+            <div>
+              <h3>Sushi</h3>
+              <span>$22.99</span> <span>x 3</span>
+            </div>
+            <div>
+              <button>−</button>
+              <button>+</button>
+            </div>
+          </li>
+
+          <li>
+            <div>
+              <h3>Sushi</h3>
+              <span>$22.99</span> <span>x 3</span>
+            </div>
+            <div>
+              <button>−</button>
+              <button>+</button>
+            </div>
+          </li>
+        </ul>
+
+        <div className={styles.totalAmount}>
+          <span>Total Amount</span>
+          <span>$100</span>
         </div>
-        <div>
-          <button>+</button>
-          <button>-</button>
-        </div>
+       
+       <div className={styles.closeOrder}>
+        <button>Close</button>
+        <button>Order</button>
+       </div>
         
       </div>
+
+      
     
     </div>
   )
