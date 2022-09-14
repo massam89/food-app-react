d4315a5a9934bc49ebc0dc55736d80f688d96c2c
Masoud <mas.sam1368@gmail.com>
2022-09-14 09:30:21 +0430

    add transition to card in header

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,20 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Context } from '../context/ContextProvider';
 import styles from './Card.module.css'
 
 const Card = () => {
   const {cardState, onDisplay} = useContext(Context);
+  const [trans, setTrans] = useState(false)
+
+  useEffect(() => {
+    setTrans(true)
+    setTimeout(() => {
+      setTrans(false)
+    }, 100)
+  }, [cardState.card])
 
   return (
-    <div onClick={onDisplay} className={styles.card}>
+    <div onClick={onDisplay} className={`${styles.card} ${trans ? styles.transition: ''}`}>
       <i className="fa-solid fa-cart-shopping"></i>
       <span>Your Card</span>
       <span>{cardState.card.length}</span>
